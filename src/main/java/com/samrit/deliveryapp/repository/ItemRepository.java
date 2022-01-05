351aa6f5982c4ec5145f32779192cccdd419f640
Samrit <hairaer02@gmail.com>
2022-01-05 11:10:18 +0300

    first Commit

---
+++ src/main/java/com/samrit/deliveryapp/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.samrit.deliveryapp.repository;
+
+import com.samrit.deliveryapp.model.Category;
+import com.samrit.deliveryapp.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    //Optional<Category> findByDescription(String description);
+
+}
